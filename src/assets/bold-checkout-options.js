25eae9ba5feae9369459892544ed4bafc99012d3
Richie Arnold <richard@ambercouch.co.uk>
2019-05-29 09:50:32 +0100

    Added bibado theme files

---
+++ src/assets/bold-checkout-options.js
@@ -0,0 +1,2 @@
+window.BOLD = window.BOLD || {};
+window.BOLD.checkout_features_defaults=[{"name":"Multi-Currency","require":true,"id":"ad4bf341-d030-11e6-b75c-0242ac110005"},{"name":"Product Upsell","require":true,"id":"bd418c3c-a207-11e7-9971-42010af00be5"},{"name":"checkout-always-on","require":true},{"name":"Shopify Discount Connector","require":true,"id":"5d8d5c43-dc24-11e7-8128-42010afe0504"}]
