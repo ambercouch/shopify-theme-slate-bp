0c77e0977cd94314532f52f4c8b49ce6b8673f5b
Richie Arnold <richard@ambercouch.co.uk>
2019-11-20 09:07:11 +0000

    Update js to fix fit vids

--- src/assets/theme-debut.js
+++ src/assets/theme-debut.js
@@ -123,40 +123,40 @@ window.slate = window.slate || {};
  * @namespace iframes
  */
 
-slate.rte = {
-  /**
-   * Wrap tables in a container div to make them scrollable when needed
-   *
-   * @param {object} options - Options to be used
-   * @param {jquery} options.$tables - jquery object(s) of the table(s) to wrap
-   * @param {string} options.tableWrapperClass - table wrapper class name
-   */
-  wrapTable: function(options) {
-    options.$tables.wrap(
-      '<div class="' + options.tableWrapperClass + '"></div>'
-    );
-  },
-
-  /**
-   * Wrap iframes in a container div to make them responsive
-   *
-   * @param {object} options - Options to be used
-   * @param {jquery} options.$iframes - jquery object(s) of the iframe(s) to wrap
-   * @param {string} options.iframeWrapperClass - class name used on the wrapping div
-   */
-  wrapIframe: function(options) {
-    options.$iframes.each(function() {
-      // Add wrapper to make video responsive
-      $(this).wrap('<div class="' + options.iframeWrapperClass + '"></div>');
-
-      // Re-set the src attribute on each iframe after page load
-      // for Chrome's "incorrect iFrame content on 'back'" bug.
-      // https://code.google.com/p/chromium/issues/detail?id=395791
-      // Need to specifically target video and admin bar
-      this.src = this.src;
-    });
-  }
-};
+// slate.rte = {
+//   /**
+//    * Wrap tables in a container div to make them scrollable when needed
+//    *
+//    * @param {object} options - Options to be used
+//    * @param {jquery} options.$tables - jquery object(s) of the table(s) to wrap
+//    * @param {string} options.tableWrapperClass - table wrapper class name
+//    */
+//   wrapTable: function(options) {
+//     // options.$tables.wrap(
+//     //   '<div class="' + options.tableWrapperClass + '"></div>'
+//     // );
+//   },
+//
+//   /**
+//    * Wrap iframes in a container div to make them responsive
+//    *
+//    * @param {object} options - Options to be used
+//    * @param {jquery} options.$iframes - jquery object(s) of the iframe(s) to wrap
+//    * @param {string} options.iframeWrapperClass - class name used on the wrapping div
+//    */
+//   wrapIframe: function(options) {
+//     options.$iframes.each(function() {
+//       // Add wrapper to make video responsive
+//       $(this).wrap('<div class="' + options.iframeWrapperClass + '"></div>');
+//
+//       // Re-set the src attribute on each iframe after page load
+//       // for Chrome's "incorrect iFrame content on 'back'" bug.
+//       // https://code.google.com/p/chromium/issues/detail?id=395791
+//       // Need to specifically target video and admin bar
+//       this.src = this.src;
+//     });
+//   }
+// };
 
 window.slate = window.slate || {};
 
@@ -2939,10 +2939,10 @@ theme.init = function() {
   // Theme-specific selectors to make tables scrollable
   var tableSelectors = '.rte table,' + '.custom__item-inner--html table';
 
-  slate.rte.wrapTable({
-    $tables: $(tableSelectors),
-    tableWrapperClass: 'scrollable-wrapper'
-  });
+  // slate.rte.wrapTable({
+  //   $tables: $(tableSelectors),
+  //   tableWrapperClass: 'scrollable-wrapper'
+  // });
 
   // Theme-specific selectors to make iframes responsive
   var iframeSelectors =
@@ -2951,10 +2951,10 @@ theme.init = function() {
     '.custom__item-inner--html iframe[src*="youtube.com/embed"],' +
     '.custom__item-inner--html iframe[src*="player.vimeo"]';
 
-  slate.rte.wrapIframe({
-    $iframes: $(iframeSelectors),
-    iframeWrapperClass: 'video-wrapper'
-  });
+  // slate.rte.wrapIframe({
+  //   $iframes: $(iframeSelectors),
+  //   iframeWrapperClass: 'video-wrapper'
+  // });
 
   // Common a11y fixes
   slate.a11y.pageLinkFocus($(window.location.hash));
